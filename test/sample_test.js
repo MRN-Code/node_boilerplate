0e6a505367f74a3cee7ab886d3786c40bfd20ec9
Dylan Wood <dwood@mrn.org>
2015-09-05 21:23:29 +0000

    switch to chai, update deps

--- test/sample_test.js
+++ test/sample_test.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var should = require('should');
+var chai = require('chai');
+
+// Decorate all objects and primitives with a `.should` method
+chai.should();
 
 describe('Array', function() {
     describe('#indexOf()', function() {
@@ -9,13 +12,13 @@ describe('Array', function() {
             testArray = [1, 2, 3];
         });
         it('should return the index when the value is present', function() {
-            testArray.indexOf(1).should.be.equal(0);
-            testArray.indexOf(2).should.be.equal(1);
-            testArray.indexOf(3).should.be.equal(2);
+            testArray.indexOf(1).should.equal(0);
+            testArray.indexOf(2).should.equal(1);
+            testArray.indexOf(3).should.equal(2);
         });
         it('should return -1 when the value is not present', function() {
-            testArray.indexOf(5).should.be.equal(-1);
-            testArray.indexOf(0).should.be.equal(-1);
+            testArray.indexOf(5).should.equal(-1);
+            testArray.indexOf(0).should.equal(-1);
         });
         it('silly async test', function(done) {
             process.nextTick(function() {
